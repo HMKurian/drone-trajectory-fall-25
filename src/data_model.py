737d79ef3ca90ced877a22f91ad1c179eef9d879
HMKurian <HANNAH.KURIAN48@QMAIL.CUNY.EDU>
2025-09-21 18:17:12 -0400

    dataspec model with validation

--- src/data_model.py
+++ src/data_model.py
@@ -1,11 +1,31 @@
 """Data models for the camera and user specification."""
-
-
+from dataclasses import dataclass
+@dataclass
 class DatasetSpec:
     """
     Data model for specifications of an image dataset.
     """
 
+    overlap: float
+    sidelap: float
+    height: float
+    scan_dimension_x: float
+    scan_dimension_y: float
+    exposure_time_ms: int
+
+    def __post_init__(self):
+        if not (0 <= self.overlap <= 1):
+            raise ValueError(f"Overlap must be between 0 and 1")
+        if not (0 <= self.sidelap <= 1):
+            raise ValueError(f"Sidelap must be between 0 and 1")
+        if self.height <= 0:
+            raise ValueError("Height must be positive")
+        if self.scan_dimension_x <= 0:
+            raise ValueError("Scan dimension x must be positive")
+        if self.scan_dimension_y <= 0:
+            raise ValueError("Scan dimension y must be positive")
+        if self.exposure_time_ms <= 0:
+            raise ValueError("Exposure time must be positive")
     pass
 
 
